c567fd91ab3370ddab9924462f84500ffdfcb94c
Quentin Geerts <quentin.geerts@bstorm.be>
2026-08-26 11:44:38 +0200

    Correction - Exercice 01 : Introduction

---
+++ README.md
@@ -0,0 +1,20 @@
+# Formation PHP - Orienté Objet
+
+
+## Table des matières
+
+1. Introduction: Programmation fonctionnelle vs orienté objet
+2. Les classes
+3. Les propriétés
+4. Les méthodes
+5. Le constructeur 
+6. L'encapsulation
+7. L'héritage
+
+Exercice récapitulatif: Panier et produits
+
+[BONUS]
+
+8. Les classes abstraites
+9. Le mot-clef "static"
+10. Les interfaces
---
+++ demo01_introduction/exercices/exercice01.php
@@ -0,0 +1,27 @@
+<?php
+
+// Dans les phrases suivantes, indiquez ce qui correspond à une classe et ce qui correspond à un objet :
+
+// - « Une voiture possède une marque et une couleur. »
+// → Classe car on parle des voitures en général, du modèle avec ses caractéristiques
+
+// - « Ma Clio rouge est garée devant chez moi. »
+// → Objet car on parle d'un objet précis en donnant une valeur à une caractéristique
+
+// - « Le compte bancaire de M. Dupont contient 250 €. »
+// → Objet car on parle d'un objet précis en donnant une valeur à une caractéristique
+
+// - « Un compte bancaire permet de déposer et de retirer de l'argent. »
+// → Classe car on parle des comportements d'un compte bancaire en général
+
+// On souhaite modéliser un téléphone portable. Proposez :
+// 3 propriétés (des données): marque, modèle, niveau_batterie, largeur, ...
+// 2 méthodes (des actions): telephoner(), envoyer_sms(), eteindre(), ouvrir_application(), ...
+
+// Question de vocabulaire : quelle est la différence entre une propriété et une méthode ?
+
+// Une propriété est une donnée (caractéristique), elle décrit ce que l'objet est.
+// C'est une variable rangée dans l'objet.
+
+// Une méthode est une action (comportement) : elle décrit ce que l'objet fait.
+// C'est une fonction rangée dans l'objet.
